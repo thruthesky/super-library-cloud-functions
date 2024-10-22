ae864313be94fb7c7ca7cfa9d470247cb0c7f4c6
JaeHo Song <thruthesky@gmail.com>
2024-10-22 18:16:37 +0900

    update on test

--- send-push-notifications/functions/index.js
+++ send-push-notifications/functions/index.js
@@ -14,6 +14,9 @@ const admin = require("firebase-admin");
 const region = "asia-southeast1"
 
 
+const batchCount = 3;
+
+
 exports.makeuppercase = onValueCreated(
     {
         ref: "/chat/messages/{roomId}/{messageId}",
@@ -61,12 +64,175 @@ exports.makeuppercase = onValueCreated(
         const messageId = event.params.messageId;
         console.log("roomId: ", roomId, ", messageId: ", messageId, ", data: ", data);
 
-        return sendMessages(roomId, messageId, data);
+        return sendChatMessages(roomId, messageId, data);
     },
 );
 
-exports.sendMessages = async (roomId, messageId, data) => {
-    // Do something
-    logger.log('sendMessages', roomId, messageId, data);
-    await admin.database().ref("/tmp").child(roomId).child(messageId).set(data.toUpperCase());
+/**
+ * Returns the FCM tokens of the users.
+ *
+ * @param {string | string[]} uids The user UIDs. It can be a single UID or an array of UIDs.
+ * @return {Promise<string[]>}
+ */
+const getUserTokens = async (uids) => {
+    uids = Array.isArray(uids) ? uids : [uids];
+
+    const tokens = [];
+    for (const uid of uids) {
+        const snapshot = await admin.database().ref("mirrored-fcm-tokens").orderByValue().equalTo(uid).get();
+        const userTokens = snapshot.val();
+        if (userTokens) {
+            tokens.push(...Object.keys(userTokens));
+        }
+    }
+    return tokens;
 }
+
+/**
+ * Returns the user ids of the chat room users.
+ * 
+ * @param {string} roomId The room id
+ * @returns {Promise<string[]>} returns the user ids of the chat room users.
+ */
+const getChatRoomUsers = async (roomId) => {
+    const snapshot = await admin.database().ref("chat/rooms").child(roomId).child('users').get();
+    const userMap = snapshot.val();
+    const uids = Object.keys(userMap);
+    return uids;
+}
+
+
+/**
+ * Returns the message batches.
+ * 
+ * @param {*} tokensArr the user tokens in an array
+ * @param {*} title title of the message
+ * @param {*} body body of the message
+ * @param {*} imageUrl url of the image. If it is an image, then apply it to the imageUrl. If there is not url, then apply user's photoUrl.
+ * @param {*} sound the sound of the push notification
+ * @param {*} parameterData page parameter data to be consumed by the flutterflow app
+ * @param {*} initialPageName page name to be opened by the flutterflow app
+ * @returns {Array} returns the message batches
+ */
+const getPayloads = (tokensArr, title, body, imageUrl, sound, parameterData, initialPageName) => {
+    var messageBatches = [];
+    for (let i = 0; i < tokensArr.length; i += batchCount) {
+        const tokensBatch = tokensArr.slice(i, Math.min(i + batchCount, tokensArr.length));
+        const messages = {
+            notification: {
+                title,
+                body,
+                ...(imageUrl && { imageUrl: imageUrl }),
+            },
+            data: {
+                initialPageName,
+                parameterData
+            },
+            android: {
+                notification: {
+                    ...(sound && { sound: sound }),
+                },
+            },
+            apns: {
+                payload: {
+                    aps: {
+                        ...(sound && { sound: sound }),
+                    },
+                },
+            },
+            tokens: tokensBatch,
+        };
+        messageBatches.push(messages);
+    }
+    return messageBatches;
+}
+
+
+
+/**
+ * Send messages to the users when there is a new message in the chat room.
+ * 
+ * @param {*} roomId the chat room id
+ * @param {*} messageId the chat message id
+ * @param {*} data the chat message data
+ */
+const sendChatMessages = async (roomId, messageId, data) => {
+    console.log('sendChatMessages:: ', roomId, messageId, data);
+
+    const uids = await getChatRoomUsers(roomId);
+    const tokens = await getUserTokens(uids);
+
+    console.log('tokens: ', tokens);
+
+    const isSingleChat = roomId.indexOf('---') >= 0;
+    const groupChat = !isSingleChat;
+
+
+    // const notificationData = data;
+    let title = data.displayName || "Unknown user";
+    if (groupChat) {
+        // get chat room name
+        // const snapshot = await admin.database().ref("chat/rooms").child(roomId).child('name').get();
+        // TODO: get chat room name and append it to the title
+        title += " (room name)";
+    }
+    const body = data.text || "...";
+    // TODO: (1) if the url is an image, then apply it to the imageUrl. (2) if there is not url, then apply user's photoUrl.
+    const imageUrl = data.url || data.photourl || "";
+    const sound = data.notification_sound || "";
+
+    // TODO: support it works with FlutterFlow
+    const parameterData = data.parameter_data || "";
+    // TODO: support it works with FlutterFlow
+    const initialPageName = data.initial_page_name || "";
+
+
+    const messageBatches = getPayloads(tokens, title, body, imageUrl, sound, parameterData, initialPageName);
+
+
+    await sendPushNotifications(messageBatches, '/chat/rooms/' + roomId + '/messages/' + messageId);
+
+
+}
+
+/**
+ * Send push notifications to the users.
+ * 
+ * @param {*} messageBatches the message batches of the payload
+ */
+const sendPushNotifications = async (messageBatches, id) => {
+    var numSent = 0;
+    var numFailed = 0;
+
+    const ref = admin.database().ref('fcm-results').push();
+    await ref.set({
+        id,
+        status: "started",
+        startedAt: new Date().toISOString(),
+    });
+
+    await Promise.all(
+        messageBatches.map(async (messages) => {
+            console.log(messages.tokens.length, "messages to send");
+            const response = await admin.messaging().sendEachForMulticast(messages);
+            numSent += response.successCount;
+            numFailed += response.failureCount;
+        })
+    );
+
+    await ref.update({
+        status: "succeeded",
+        num_sent: numSent,
+        num_failed: numFailed,
+        finishedAt: new Date().toISOString(),
+    });
+}
+
+
+exports.getChatRoomUsers = getChatRoomUsers;
+exports.getUserTokens = getUserTokens;
+exports.sendChatMessages = sendChatMessages;
+exports.getPayloads = getPayloads;
+exports.sendPushNotifications = sendPushNotifications;
+
+
--- send-push-notifications/functions/test.js
+++ send-push-notifications/functions/test.js
@@ -1,11 +1,142 @@
 
-const { onValueWritten, onValueCreated } = require("firebase-functions/v2/database");
-const logger = require("firebase-functions/logger");
-const admin = require("firebase-admin");
 
-// Choose one of "us-central1", "us-east1", "europe-west1", "asia-southeast1" for the Realtime Database access.
+
+const admin = require("firebase-admin");
+const { sendChatMessages, getChatRoomUsers, getUserTokens, getPayloads, sendPushNotifications } = require(".");
+const { describe, test, after } = require("node:test");
+const assert = require("assert");
+
+
+const tokens = {
+    'S6lXfLJErXQLQUvXcmu9d8wbJ6D3': [
+        'dV9IpnMaik5nrShSu7pNis:APA91bGOPjbh8gD6EmLs-1FwiM-1gTaPR3h014bFkjP-q1PDYgtssnk-zjsPj-w98Cy0CK2O3MBGMHZgrFtQoX8E9TZRrKJ0hGAaQIT8iT8r8_4HYLfk-O3O32zyQvVPC2chDR3MB9Rt',
+        '--wrong-token--',
+    ],
+    'user-A': [
+        '--wrong-token--1--',
+        '--wrong-token--2--',
+    ],
+    'user-B': [
+        '--wrong-token--3--',
+        '--wrong-token--4--',
+        '--wrong-token--B-5--',
+    ],
+};
+
+const extraTokens = {
+    'user-C': [
+        '--wrong-token--5--',
+        '--wrong-token--6--',
+    ],
+    'user-D': [
+        '--wrong-token--7--',
+        '--wrong-token--8--',
+    ],
+};
+
 
 if (admin.apps.length === 0) {
-    admin.initializeApp();
+    admin.initializeApp({
+        databaseURL: 'http://127.0.0.1:9000/?ns=withcenter-test-4-default-rtdb',
+    });
 }
+
+
+describe("sendChatMessages", () => {
+
+
+    test("should send messages", async () => {
+        // Prepare: Add users into the chat room.
+        const roomId = "group-chat" + (new Date).getTime();
+        const messageId = "message-1";
+        Object.keys(tokens).forEach(async (uid) => {
+            await admin.database().ref("chat/rooms").child(roomId).child('users').child(uid).set(true);
+        });
+        Object.keys(extraTokens).forEach(async (uid) => {
+            await admin.database().ref("chat/rooms").child('room-Extra').child('users').child(uid).set(true);
+        });
+
+
+        // Prepare: Add FCM tokens for the users in [tokens, extraTokens]
+        Object.keys(tokens).forEach(async (uid) => {
+            const userTokens = tokens[uid];
+            userTokens.forEach(async (token) => {
+                await admin.database().ref("mirrored-fcm-tokens").child(token).set(uid);
+            });
+        });
+        Object.keys(extraTokens).forEach(async (uid) => {
+            const userTokens = extraTokens[uid];
+            userTokens.forEach(async (token) => {
+                await admin.database().ref("mirrored-fcm-tokens").child(token).set(uid);
+            });
+        });
+
+
+        // Test parts: get users of the chat room
+        const uids = await getChatRoomUsers(roomId);
+        assert.deepStrictEqual(uids, Object.keys(tokens));
+
+
+        // Test parts: ge the tokens of the users
+        const aTokens = await getUserTokens('user-A');
+        assert.deepStrictEqual(aTokens, [
+            '--wrong-token--1--',
+            '--wrong-token--2--',
+        ]);
+        const bTokens = await getUserTokens('user-B');
+        assert.deepStrictEqual(bTokens, [
+            '--wrong-token--3--',
+            '--wrong-token--4--',
+            '--wrong-token--B-5--',
+        ]);
+
+        // Get all the tokens of the users in the chat room.
+        const allTokens = await getUserTokens(uids);
+        // Get all the values of the keys in the tokens into an array
+        const allTokensValues = [];
+        Object.keys(tokens).forEach((uid) => {
+            allTokensValues.push(...tokens[uid]);
+        });
+
+        // Test allTokens and allTokensValues. Both of the are arrays. Test if they have the same values.
+        assert.deepStrictEqual(allTokens.sort(), allTokensValues.sort());
+
+
+
+        // TODO: do the `getPayloads` message tests;
+        // `const messageBatches = getPayloads(tokens, title, body, imageUrl, sound, parameterData, initialPageName);`
+
+
+        const messageBatches = getPayloads(allTokensValues, 'title', 'body', 'imageUrl', 'sound', 'parameterData', 'initialPageName');
+        assert(messageBatches.length == 3);
+
+
+        await sendPushNotifications(messageBatches, '/chat/rooms/' + roomId + '/messages/' + messageId);
+
+
+
+        // const data = {
+        //     senderUid: 'abc',
+        //     text: "Hello, world! - 34",
+        // };
+        // await sendChatMessages(roomId, messageId, data);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
